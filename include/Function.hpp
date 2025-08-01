57474a50fa0e36f73406f11dccfecaaca2b92269
ThomasJ1991 <kgjiang19911101@gmail.com>
2025-07-31 22:15:58 -0400

    Create Function.hpp

---
+++ include/Function.hpp
@@ -0,0 +1,25 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+
+#include <string>
+#include <stdexcept>
+
+class Function {
+public:
+    // Returns g(x), throws exception on invalid input
+    double g(double x) const;
+
+    // Returns g'(x), throws exception on invalid input
+    double g_derivative(double x) const;
+
+    // Returns f(x), throws exception on invalid input
+    double f(double x) const;
+
+    // Returns f'(x), throws exception on invalid input
+    double f_derivative(double x) const;
+
+    // Returns symbolic string
+    std::string to_string() const;
+};
+
+#endif // FUNCTION_HPP
