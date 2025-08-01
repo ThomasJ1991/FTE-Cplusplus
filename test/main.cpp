298fcd78fcc1009bd4b739fab6b28d0ad56bebf6
ThomasJ1991 <kgjiang19911101@gmail.com>
2025-08-01 00:10:12 -0400

    User could try multiple inputs

--- test/main.cpp
+++ test/main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <cctype>
 #include "Function.hpp"
 
 int main() {
     Function f;
     double x;
-
+    char choice;
+    
     std::cout << f.to_string() << "\n";
     std::cout << "Enter x: ";
     std::cin >> x;
 
-    try {
-        double fx = f.evaluate(x);
-        double dfx = f.derivative(x);
-        std::cout << "The function value f(" << x << ") = " << fx << "\n";
-        std::cout << "The derivative of the function f'(" << x << ") = " << dfx << "\n";
-    } catch (const std::domain_error& e) {
-        std::cerr << "Error: " << e.what() << "\n";
-    }
+    do {
+        std::cout << "\nEnter a value for x: ";
+        std::cin >> x;
 
+        try {
+            double fx = f.evaluate(x);
+            double dfx = f.derivative(x);
+            std::cout << "f(" << x << ") = " << fx << "\n";
+            std::cout << "f'(" << x << ") = " << dfx << "\n";
+        } catch (const std::domain_error& e) {
+            std::cerr << "Error: " << e.what() << "\n";
+        }
+
+        std::cout << "\nContinue with a new input x? (y/n): ";
+        std::cin >> choice;
+
+    } while (toppper(choice) == 'Y');
+
+    std::cout << "End of the function\n";
     return 0;
 }
